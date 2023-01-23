5ec5f61e36a9f9ebef32abb0dfa7e81f440325ab
unknown <prabinbk06@gmail.com>
2023-01-23 16:51:40 +0545

    complete-design

--- Assets/jQuery/index.js
+++ Assets/jQuery/index.js
@@ -1,7 +1,9 @@
+// client slider
 $('.client-carousel').owlCarousel({
     stagePadding: 20,
     loop: true,
     margin: 60,
+    startPosition: 3,
     nav: true,
     center: true,
     autoplay: true,
@@ -13,12 +15,15 @@ $('.client-carousel').owlCarousel({
             items: 5
         }
     }
-})
+});
+
+// testimonial slider
 $('.testimonials_wrapper').owlCarousel({
     stagePadding: 20,
     loop: true,
     margin: 260,
-    merge:true,
+    merge: true,
+    startPosition: 2,
     center: true,
     responsive: {
         0: {
@@ -29,6 +34,121 @@ $('.testimonials_wrapper').owlCarousel({
         },
         800: {
             items: 5
+        }
+    }
+});
+
+// television slider
+$('.tv_wrapper').owlCarousel({
+    stagePadding: 110,
+    loop: true,
+    margin: 58,
+    startPosition: 2,
+    center: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        800: {
+            items: 3
+        }
+    }
+});
+// campaign slider
+$('.campaign_wrapper').owlCarousel({
+    stagePadding: 110,
+    loop: true,
+    startPosition: 2,
+    margin: 58,
+    center: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        800: {
+            items: 3
+        }
+    }
+});
+// digital campaign slider 
+$('.img_wrapper').owlCarousel({
+    stagePadding: 20,
+    startPosition: 2,
+    loop: true,
+    margin: 25,
+    center: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        800: {
+            items: 3
+        }
+    }
+})
+// marketing slider 
+$('.marketing_wrapper').owlCarousel({
+    stagePadding: 20,
+    loop: true,
+    startPosition: 2,
+    margin: 25,
+    center: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        800: {
+            items: 3
+        }
+    }
+});
+// showcase slider 
+$('.showcase_wrapper').owlCarousel({
+    stagePadding: 110,
+    loop: true,
+    margin: 58,
+    startPosition: 2,
+    center: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        800: {
+            items: 3
+        }
+    }
+})
+// brand slider 
+$('.brand_wrap').owlCarousel({
+    stagePadding: 110,
+    startPosition: 2,
+    loop: true,
+    margin: 58,
+    center: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        800: {
+            items: 3
         }
     }
 })
