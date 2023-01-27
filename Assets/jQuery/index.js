f0a66ed34f500e008d2607bebade3a0a8dbdc5e4
unknown <prabinbk06@gmail.com>
2023-01-27 12:00:21 +0545

    complete

--- Assets/jQuery/index.js
+++ Assets/jQuery/index.js
@@ -1,176 +1,145 @@
-// client slider
-$('.client-carousel').owlCarousel({
-    stagePadding: 20,
-    loop: true,
-    margin: 20,
-    startPosition: 2,
-    nav: true,
-    center: true,
-    autoplay: true,
-    responsive: {
-        0: {
-            items: 2,
-            stagePadding: 0,
-            margin:0,
+$(document).ready(function () {
 
-        },
-         350: {
-            items: 3,
-            // stagePadding: 9,
-            // margin:90,
+    // client slider
+    $('.client-carousel').owlCarousel({
+        stagePadding: 20,
+        loop: true,
+        margin: 20,
+        startPosition: 2,
+        center: true,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 2,
+            },
+            350: {
+                items: 3,
+                stagePadding: 60,
+                margin: 50,
 
-        },
-        600: {
-            items: 5,
-            // margin:120
-        },
-        800: {
-            items: 5,
+            },
+            600: {
+                items: 5,
+            },
+            800: {
+                items: 5,
+            }
         }
-    }
-});
+    });
 
-// testimonial slider
-$('.testimonials_wrapper').owlCarousel({
-    stagePadding: 78,
-    loop: true,
-    margin: 280,
-    merge: true,
-    center: true,
-    responsive: {
-        0: {
-            items: 1,
-            margin: 200,
-            statePadding: 50,
-        }, 
-        350: {
-            items: 1,
-            margin: 130,
-            statePadding: 340,
-        },
-        600: {
-            items: 1,
-            margin: 450,
-            stagePadding: 45,
+    // testimonial slider
+    $('.testimonials_wrapper').owlCarousel({
+        loop: true,
+        merge: true,
+        nav: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            800: {
+                items: 2,
+            },
+            1200: {
+                items: 3
+            }
+        }
+    });
 
-        },
-        800: {
-            items: 5
+    //TV slider
+    $('.tv_wrapper').owlCarousel({
+        loop: true,       
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2,
+            },
+             1200: {
+                items: 3,
+                margin:10,
+            },
         }
-    }
-});
+    });
+    // campaign slider
+    $('.campaign_wrapper').owlCarousel({
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
 
-// television slider
-$('.tv_wrapper').owlCarousel({
-    stagePadding: 110,
-    loop: true,
-    margin: 58,
-    startPosition: 2,
-    center: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        800: {
-            items: 3
+            800: {
+                items: 2,
+            },
+             1200: {
+                items: 3,
+                margin:10,
+            },
         }
-    }
-});
-// campaign slider
-$('.campaign_wrapper').owlCarousel({
-    stagePadding: 110,
-    loop: true,
-    startPosition: 2,
-    margin: 58,
-    center: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        800: {
-            items: 3
+    });
+    // digital campaign slider 
+    $('.img_wrapper').owlCarousel({
+        loop: true,       
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 1,
+            },
+             1200: {
+                items: 3,
+            },
         }
-    }
-});
-// digital campaign slider 
-$('.img_wrapper').owlCarousel({
-    stagePadding: 20,
-    startPosition: 2,
-    loop: true,
-    margin: 25,
-    center: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        800: {
-            items: 3
+    })
+    // marketing slider 
+    $('.marketing_wrapper').owlCarousel({
+        loop: true,       
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 1,
+            },
+             1200: {
+                items: 3,
+            },
         }
-    }
-})
-// marketing slider 
-$('.marketing_wrapper').owlCarousel({
-    stagePadding: 20,
-    loop: true,
-    startPosition: 2,
-    margin: 25,
-    center: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        800: {
-            items: 3
+    });
+    // showcase slider 
+    $('.showcase_wrapper').owlCarousel({
+        loop: true,       
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2,
+            },
+             1200: {
+                items: 3,
+                margin:10,
+            },
         }
-    }
-});
-// showcase slider 
-$('.showcase_wrapper').owlCarousel({
-    stagePadding: 110,
-    loop: true,
-    margin: 58,
-    startPosition: 2,
-    center: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        800: {
-            items: 3
+    })
+    // brand slider 
+    $('.brand_wrap').owlCarousel({
+        loop: true,       
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2,
+            },
+             1200: {
+                items: 3,
+                margin:10,
+            },
         }
-    }
-})
-// brand slider 
-$('.brand_wrap').owlCarousel({
-    stagePadding: 110,
-    startPosition: 2,
-    loop: true,
-    margin: 58,
-    center: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        800: {
-            items: 3
-        }
-    }
+    })
+
 })
